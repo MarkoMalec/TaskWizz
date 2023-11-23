24f2dfdbfa5eefc0678ee6777e4df69b2603cffa
Marko Malec <cropsychooo@yahoo.com>
2023-11-23 12:51:49 +0100

    improved loading component

--- src/app/admin/tasks/loading.tsx
+++ src/app/admin/tasks/loading.tsx
@@ -1,32 +1,52 @@
-import React from 'react';
+import React from "react";
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableCell
-  } from "~/components/ui/table";
-  import { Skeleton } from '~/components/ui/skeleton';
+  Table,
+  TableBody,
+  TableCaption,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "~/components/ui/table";
+import { Skeleton } from "~/components/ui/skeleton";
 
 const loading = () => {
   return (
     <Table>
-        <TableHeader>
-            <TableHead><Skeleton className="w-[150px] h-4" /></TableHead>
-            <TableHead><Skeleton className="w-[150px] h-4" /></TableHead>
-            <TableHead><Skeleton className="w-[150px] h-4" /></TableHead>
-            <TableHead><Skeleton className="w-[150px] h-4" /></TableHead>
-        </TableHeader>
-        <TableBody>
-            <TableCell><Skeleton className="w-[150px] h-4" /></TableCell>
-            <TableCell><Skeleton className="w-[150px] h-4" /></TableCell>
-            <TableCell><Skeleton className="w-[150px] h-4" /></TableCell>
-            <TableCell><Skeleton className="w-[150px] h-4" /></TableCell>
-        </TableBody>
-        </Table>
-  )
-}
+      <TableHeader>
+        <TableRow>
+          <TableHead>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableHead>
+          <TableHead>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableHead>
+          <TableHead>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableHead>
+          <TableHead>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-4 w-[150px]" />
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
 
 export default loading;
