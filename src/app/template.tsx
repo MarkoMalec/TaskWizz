e69fcdd1889361b055b4c7c89d05b6ba23c1ad7a
Marko Malec <cropsychooo@yahoo.com>
2023-11-23 09:10:30 +0100

    .

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/server/auth";
 import { prisma } from "~/lib/prisma";
+import { redirect } from "next/navigation";
 import Header from "~/components/elements/Header/Header";
 
 export default async function Template({
@@ -10,7 +11,7 @@ export default async function Template({
 }) {
   const session = await getServerSession(authOptions);
   if (!session) {
-    return <>Loading...</>;
+    redirect('/signin');
   }
 
   const user = await prisma.user.findUnique({
