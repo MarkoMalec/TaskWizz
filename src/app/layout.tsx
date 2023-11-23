c42442ef8464b63989cb0bf6e43cad354464b736
Marko Malec <cropsychooo@yahoo.com>
2023-11-23 09:38:16 +0100

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`min-h-screen font-sans ${inter.variable} container`}>
+      <body className={`min-h-screen font-sans ${inter.variable}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
